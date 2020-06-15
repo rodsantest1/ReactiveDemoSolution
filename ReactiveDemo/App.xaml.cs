84434e4b8a324dcf9ad3fade2a4c6fd7535bbfc6
Rodney Santiago <rodsan.724@outlook.com>
2020-06-14 20:06:42 -0400

    got it working!

--- ReactiveDemo/App.xaml.cs
+++ ReactiveDemo/App.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using ReactiveUI;
+using Splat;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Windows;
 
 namespace ReactiveDemo
@@ -14,7 +17,10 @@ namespace ReactiveDemo
     {
         public App()
         {
-           this.InitializeComponent();
+            // A helper method that will register all classes that derive off IViewFor 
+            // into our dependency injection container. ReactiveUI uses Splat for it's 
+            // dependency injection by default, but you can override this if you like.
+            Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetCallingAssembly());
         }
     }
 }
