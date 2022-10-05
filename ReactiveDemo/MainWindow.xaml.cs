bf403b190ccbc901fc08039bc6561244273eefb7
Rodney Santiago <rodney.santiago@ups.com>
2022-10-05 14:19:37 -0400

    restoring original code to main

--- ReactiveDemo/MainWindow.xaml.cs
+++ ReactiveDemo/MainWindow.xaml.cs
@@ -1,53 +1,32 @@
-﻿using ReactiveUI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reactive.Disposables;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Reactive.Disposables;
+using ReactiveUI;
 
 namespace ReactiveDemo
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     // MainWindow class derives off ReactiveWindow which implements the IViewFor<TViewModel>
     // interface using a WPF DependencyProperty. We need this to use WhenActivated extension
     // method that helps us handling View and ViewModel activation and deactivation.
-    public partial class MainWindow : ReactiveWindow<AppViewModel>
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
             ViewModel = new AppViewModel();
 
-            // We create our bindings here. These are the code behind bindings which allow 
+            // We create our bindings here. These are the code behind bindings which allow
             // type safety. The bindings will only become active when the Window is being shown.
-            // We register our subscription in our disposableRegistration, this will cause 
+            // We register our subscription in our disposableRegistration, this will cause
             // the binding subscription to become inactive when the Window is closed.
-            // The disposableRegistration is a CompositeDisposable which is a container of 
-            // other Disposables. We use the DisposeWith() extension method which simply adds 
+            // The disposableRegistration is a CompositeDisposable which is a container of
+            // other Disposables. We use the DisposeWith() extension method which simply adds
             // the subscription disposable to the CompositeDisposable.
             this.WhenActivated(disposableRegistration =>
             {
                 // Notice we don't have to provide a converter, on WPF a global converter is
                 // registered which knows how to convert a boolean into visibility.
                 this.OneWayBind(ViewModel,
-                    viewModel => viewModel.IsBusy,
+                    viewModel => viewModel.IsAvailable,
                     view => view.searchResultsListBox.Visibility)
-                    .DisposeWith(disposableRegistration);
-
-                this.OneWayBind(ViewModel,
-                    viewModel => viewModel.IsBusy,
-                    view => view.BusyLabel.Visibility)
                     .DisposeWith(disposableRegistration);
 
                 this.OneWayBind(ViewModel,
--- ReactiveDemo/ViewModels/AppViewModel.cs
+++ ReactiveDemo/ViewModels/AppViewModel.cs
@@ -1,39 +1,28 @@
-﻿
-
-using DynamicData;
-using NuGet.Configuration;
-using NuGet.Protocol.Core.Types;
-using ReactiveUI;
-using ReactiveUI.Fody.Helpers;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
-using System.Reactive;
-using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using NuGet.Configuration;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using ReactiveUI;
 
 namespace ReactiveDemo
 {
     // AppViewModel is where we will describe the interaction of our application.
-    // We can describe the entire application in one class since it's very small now. 
-    // Most ViewModels will derive off ReactiveObject, while most Model classes will 
+    // We can describe the entire application in one class since it's very small now.
+    // Most ViewModels will derive off ReactiveObject, while most Model classes will
     // most derive off INotifyPropertyChanged
-    public class AppViewModel : ReactiveObject, IActivatableViewModel
+    public class AppViewModel : ReactiveObject
     {
-        private readonly SourceList<NugetDetailsViewModel> _searchResultsSource = new SourceList<NugetDetailsViewModel>();
-        private ReadOnlyObservableCollection<NugetDetailsViewModel> _searchResults;
-        public ReadOnlyObservableCollection<NugetDetailsViewModel> SearchResults => _searchResults;
-
-        public ReactiveCommand<string, Unit> SearchCommand { get; }
-
         // In ReactiveUI, this is the syntax to declare a read-write property
-        // that will notify Observers, as well as WPF, that a property has 
-        // changed. If we declared this as a normal property, we couldn't tell 
+        // that will notify Observers, as well as WPF, that a property has
+        // changed. If we declared this as a normal property, we couldn't tell
         // when it has changed!
         private string _searchTerm;
+
         public string SearchTerm
         {
             get => _searchTerm;
@@ -43,126 +32,92 @@ namespace ReactiveDemo
         // Here's the interesting part: In ReactiveUI, we can take IObservables
         // and "pipe" them to a Property - whenever the Observable yields a new
         // value, we will notify ReactiveObject that the property has changed.
-        // 
+        //
         // To do this, we have a class called ObservableAsPropertyHelper - this
         // class subscribes to an Observable and stores a copy of the latest value.
         // It also runs an action whenever the property changes, usually calling
         // ReactiveObject's RaisePropertyChanged.
+        private readonly ObservableAsPropertyHelper<IEnumerable<NugetDetailsViewModel>> _searchResults;
 
-        //public IEnumerable<NugetDetailsViewModel> SearchResults { [ObservableAsProperty] get; }
+        public IEnumerable<NugetDetailsViewModel> SearchResults => _searchResults.Value;
 
-        // Here, we want to create a property to represent when the application 
-        // is performing a search (i.e. when to show the "spinner" control that 
+        // Here, we want to create a property to represent when the application
+        // is performing a search (i.e. when to show the "spinner" control that
         // lets the user know that the app is busy). We also declare this property
-        // to be the result of an Observable (i.e. its value is derived from 
+        // to be the result of an Observable (i.e. its value is derived from
         // some other property)
-        public bool IsBusy { [ObservableAsProperty] get; }
+        private readonly ObservableAsPropertyHelper<bool> _isAvailable;
 
-        public ViewModelActivator Activator { get; }
+        public bool IsAvailable => _isAvailable.Value;
 
         public AppViewModel()
         {
-            Activator = new ViewModelActivator();
-
-            SearchCommand = ReactiveCommand.CreateFromObservable<string, Unit>(this.SearchNuGetPackages);
-
-
             // Creating our UI declaratively
-            // 
-            // The Properties in this ViewModel are related to each other in different 
+            //
+            // The Properties in this ViewModel are related to each other in different
             // ways - with other frameworks, it is difficult to describe each relation
-            // succinctly; the code to implement "The UI spinner spins while the search 
+            // succinctly; the code to implement "The UI spinner spins while the search
             // is live" usually ends up spread out over several event handlers.
             //
-            // However, with ReactiveUI, we can describe how properties are related in a 
-            // very organized clear way. Let's describe the workflow of what the user does 
+            // However, with ReactiveUI, we can describe how properties are related in a
+            // very organized clear way. Let's describe the workflow of what the user does
             // in this application, in the order they do it.
 
             // We're going to take a Property and turn it into an Observable here - this
             // Observable will yield a value every time the Search term changes, which in
-            // the XAML, is connected to the TextBox. 
+            // the XAML, is connected to the TextBox.
             //
-            // We're going to use the Throttle operator to ignore changes that happen too 
-            // quickly, since we don't want to issue a search for each key pressed! We 
-            // then pull the Value of the change, then filter out changes that are identical, 
+            // We're going to use the Throttle operator to ignore changes that happen too
+            // quickly, since we don't want to issue a search for each key pressed! We
+            // then pull the Value of the change, then filter out changes that are identical,
             // as well as strings that are empty.
             //
-            // We then do a SelectMany() which starts the task by converting Task<IEnumerable<T>> 
-            // into IObservable<IEnumerable<T>>. If subsequent requests are made, the 
-            // CancellationToken is called. We then ObserveOn the main thread, 
-            // everything up until this point has been running on a separate thread due 
+            // We then do a SelectMany() which starts the task by converting Task<IEnumerable<T>>
+            // into IObservable<IEnumerable<T>>. If subsequent requests are made, the
+            // CancellationToken is called. We then ObservableOn the main thread,
+            // everything up until this point has been running on a separate thread due
             // to the Throttle().
             //
             // We then use a ObservableAsPropertyHelper and the ToProperty() method to allow
             // us to have the latest results that we can expose through the property to the View.
+            _searchResults = this
+                .WhenAnyValue(x => x.SearchTerm)
+                .Throttle(TimeSpan.FromMilliseconds(800))
+                .Select(term => term?.Trim())
+                .DistinctUntilChanged()
+                .Where(term => !string.IsNullOrWhiteSpace(term))
+                .SelectMany(SearchNuGetPackages)
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .ToProperty(this, x => x.SearchResults);
 
-
-            // We subscribe to the "ThrownExceptions" property of our OAPH, where ReactiveUI 
-            // marshals any exceptions that are thrown in SearchNuGetPackages method. 
+            // We subscribe to the "ThrownExceptions" property of our OAPH, where ReactiveUI
+            // marshals any exceptions that are thrown in SearchNuGetPackages method.
             // See the "Error Handling" section for more information about this.
-
-            //_searchResults.ThrownExceptions.Subscribe(error => { /* Handle errors here */ });
+            _searchResults.ThrownExceptions.Subscribe(error => { /* Handle errors here */ });
 
             // A helper method we can use for Visibility or Spinners to show if results are available.
             // We get the latest value of the SearchResults and make sure it's not null.
-
-            this.WhenActivated(disposables =>
-            {
-                _searchResultsSource
-                    .Connect()
-                    .ObserveOn(RxApp.MainThreadScheduler)
-                    .Bind(out _searchResults)
-                    .DisposeMany()
-                    .Subscribe()
-                    .DisposeWith(disposables);
-
-                this.WhenAnyValue(x => x.SearchTerm)
-                    .Throttle(TimeSpan.FromMilliseconds(800))
-                    .Select(term => term?.Trim())
-                    .DistinctUntilChanged()
-                    .Where(term => !string.IsNullOrWhiteSpace(term))
-                    //.SelectMany(SearchNuGetPackages)
-                    .ObserveOn(RxApp.MainThreadScheduler)
-                    .InvokeCommand(SearchCommand)
-                    //.ToPropertyEx(this, x => x.SearchResults)
-                    .DisposeWith(disposables);
-
-                this.WhenAnyObservable(x => x.SearchCommand.IsExecuting)
-                    .ObserveOn(RxApp.MainThreadScheduler)
-                    .ToPropertyEx(this, x => x.IsBusy)
-                    .DisposeWith(disposables);
-
-                //    this.WhenAnyValue(x => x.SearchResults)
-                //       .Select(searchResults => searchResults != null)
-                //       .ToPropertyEx(this, x => x.IsAvailable)
-                //       .DisposeWith(disposables);
-            });
+            _isAvailable = this
+                .WhenAnyValue(x => x.SearchResults)
+                .Select(searchResults => searchResults != null)
+                .ToProperty(this, x => x.IsAvailable);
         }
+
         // Here we search NuGet packages using the NuGet.Client library. Ideally, we should
-        // extract such code into a separate service, say, INuGetSearchService, but let's 
+        // extract such code into a separate service, say, INuGetSearchService, but let's
         // try to avoid overcomplicating things at this time.
-        private IObservable<Unit> SearchNuGetPackages(
-            string term)
+        private async Task<IEnumerable<NugetDetailsViewModel>> SearchNuGetPackages(
+            string term, CancellationToken token)
         {
-            return Observable.StartAsync(async () =>
-            {
-                var providers = new List<Lazy<INuGetResourceProvider>>();
-                providers.AddRange(Repository.Provider.GetCoreV3()); // Add v3 API support
-                var packageSource = new PackageSource("https://api.nuget.org/v3/index.json");
-                var source = new SourceRepository(packageSource, providers);
+            var providers = new List<Lazy<INuGetResourceProvider>>();
+            providers.AddRange(Repository.Provider.GetCoreV3()); // Add v3 API support
+            var package = new PackageSource("https://api.nuget.org/v3/index.json");
+            var source = new SourceRepository(package, providers);
 
-                var filter = new SearchFilter(false);
-                var resource = await source.GetResourceAsync<PackageSearchResource>().ConfigureAwait(false);
-                var metadata = await resource.SearchAsync(term, filter, 0, 10, null, default).ConfigureAwait(false);
-                var nugetDetails = metadata.Select(x => new NugetDetailsViewModel(x));
-
-                _searchResultsSource.Edit(innerList =>
-                {
-                    innerList.Clear();
-                    innerList.AddRange(nugetDetails);
-                });
-
-            }, RxApp.MainThreadScheduler);
+            var filter = new SearchFilter(false);
+            var resource = await source.GetResourceAsync<PackageSearchResource>().ConfigureAwait(false);
+            var metadata = await resource.SearchAsync(term, filter, 0, 10, new NuGet.Common.NullLogger(), token).ConfigureAwait(false);
+            return metadata.Select(x => new NugetDetailsViewModel(x));
         }
     }
 }
